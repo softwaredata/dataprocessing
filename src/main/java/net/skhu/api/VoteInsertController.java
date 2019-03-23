f127b654f99be127c541103926d90a4444d6609a
choitaehoon <xognsdl1230@naver.com>
2019-03-23 23:11:43 +0900

    배포테스트

--- src/main/java/net/skhu/api/VoteInsertController.java
+++ src/main/java/net/skhu/api/VoteInsertController.java
@@ -1,8 +1,14 @@
 package net.skhu.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VoteInsertController {
+
+    @GetMapping("test")
+    public String test() {
+        return "배포테스트";
+    }
 
 }
--- src/test/java/net/skhu/DemoApplicationTests.java
+++ src/test/java/net/skhu/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package net.skhu;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,7 +13,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 public class DemoApplicationTests {
 
     @Test
+    @DisplayName("테스트")
     public void contextLoads() {
+        
     }
 
 }
