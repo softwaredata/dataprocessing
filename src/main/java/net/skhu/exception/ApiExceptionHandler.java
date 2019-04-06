127a74284c6ace9cacd7593b30ca642a53c9782c
choitaehoon <xognsdl1230@naver.com>
2019-04-06 22:18:59 +0900

    #1 Checking for validity errors

---
+++ src/main/java/net/skhu/exception/ApiExceptionHandler.java
@@ -0,0 +1,16 @@
+package net.skhu.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleEntityNotFoundException(MethodArgumentNotValidException e){
+        return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
