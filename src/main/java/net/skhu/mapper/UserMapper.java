5ecbbb97fd3ecc8e1bb719567e1b50e33aac31bc
bomi <a51022866@gmail.com>
2019-05-03 22:38:30 +0900

    Create Controller/Service About Login Function

--- src/main/java/net/skhu/mapper/UserMapper.java
+++ src/main/java/net/skhu/mapper/UserMapper.java
@@ -1,11 +1,16 @@
-package net.skhu.Mapper;
+package net.skhu.mapper;
 
 import net.skhu.domain.Users;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
+
+    @Select("SELECT * FROM users WHERE idx = {id}")
+    Users findById(@Param("id") int idx);
 
     @Insert("insert into Users(name, studentIdx, department, password, email, type)" +
             "values(#{name}, #{studentIdx}, #{department}, #{password}, #{email}, #{type})")
