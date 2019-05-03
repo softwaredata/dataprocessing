ad908f28c12513137b2b7ee2e01a48302b9ff938
choitaehoon <xognsdl1230@naver.com>
2019-05-03 19:32:20 +0900

    #1 signUp validation check

--- src/test/java/net/skhu/service/PasswordTest.java
+++ src/test/java/net/skhu/service/PasswordTest.java
@@ -1,5 +1,6 @@
 package net.skhu.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@Slf4j
 public class PasswordTest {
 
     private PasswordEncoder passwordEncoder;
@@ -28,4 +30,5 @@ public class PasswordTest {
         String passwordTest = "test";
         assertTrue(passwordEncoder.matches(passwordTest,passwordEncoder.encode(passwordTest)));
     }
+
 }
--- src/test/java/net/skhu/service/SignUpServiceTest.java
+++ src/test/java/net/skhu/service/SignUpServiceTest.java
@@ -1,18 +1,38 @@
 package net.skhu.service;
 
+import net.skhu.domain.Users;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class SignUpServiceTest {
 
+    private  PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setup() throws Exception {
+        passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
     @Test
-    @DisplayName("회원가입 테스트")
-    public void signUpTest() {
+    @DisplayName("Users 값 없을때")
+    void throwsExceptionWhenUserInsert () {
+
+        Users users = Users.builder()
+                .name("test")
+                .studentIdx(20111111)
+                .password(passwordEncoder.encode("test"))
+                .email("test@test.com")
+                .type(1)
+                .build();
+
 
     }
 }
