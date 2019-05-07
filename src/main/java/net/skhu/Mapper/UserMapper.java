7bb76d34589ee6ffd5c95a255ae9f1bad3ac87eb
minsub <welcltm@gmail.com>
2019-05-07 13:47:46 +0900

    비밀번호 찾기 구현을 위한 초기 작업

--- src/main/java/net/skhu/Mapper/UserMapper.java
+++ src/main/java/net/skhu/Mapper/UserMapper.java
@@ -1,8 +1,11 @@
 package net.skhu.Mapper;
 
 import net.skhu.domain.Users;
+import net.skhu.dto.PwsReq;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface UserMapper {
@@ -10,4 +13,11 @@ public interface UserMapper {
     @Insert("insert into Users(name, studentIdx, department, password, email, type)" +
             "values(#{name}, #{studentIdx}, #{department}, #{password}, #{email}, #{type})")
     void insertUsers(Users users);
+
+    @Select("select * from Users where studentIdx = #{studentIdx} and email = #{email}")
+    Users findUser(Users user);
+
+
+    @Update("update Users set password = #{password} where studentIdx ={studentIdx}")
+    void updatePws(Users users);
 }
