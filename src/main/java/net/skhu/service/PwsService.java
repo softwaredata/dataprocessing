4327d919efb87aef00740a370f5fa9c71e09c914
meeansub <welcltm@gmail.com>
2019-05-10 18:38:34 +0900

    비밀번호찾기를위한구현2

--- src/main/java/net/skhu/service/PwsService.java
+++ src/main/java/net/skhu/service/PwsService.java
@@ -3,6 +3,7 @@ package net.skhu.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.skhu.dto.PwsReq;
 import net.skhu.mapper.UserMapper;
 import net.skhu.domain.Users;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,17 +21,22 @@ public class PwsService {
     public void find_pw(Users users) throws Exception {
 
         // 아이디가 없으면
-
+        if(userMapper.findUser(users.getStudentIdx())==0)
+            System.out.println("일치아이디 없음");
         // 가입에 사용한 이메일이 아니면
-
-        // 임시 비밀번호 생성
-        String pw = "";
-        for (int i = 0; i < 12; i++) {
-            pw += (char) ((Math.random() * 26) + 97);
+        else if(userMapper.findUserMatchEmail(users) ==0)
+            System.out.println("이메일 일치하지않음");
+        else{// 임시 비밀번호 생성
+            String pw = "";
+            for (int i = 0; i < 12; i++) {
+                pw += (char) ((Math.random() * 26) + 97);
+            }
+            //비밀번호인코딩
+            users.setPassword(passwordEncoder.encode(pw));
+            // 비밀번호 변경
+            userMapper.updatePws(users);
         }
-        //users.setPassword(pw);
-        // 비밀번호 변경
-        userMapper.updatePws(users);
+
         // 비밀번호 변경 메일 발송
         //send_mail(users, "find_pw");
 
@@ -38,5 +44,49 @@ public class PwsService {
         //out.close();
 
     }
+
+//    // 이메일 발송
+//    public void send_mail(Users users, String div) throws Exception {
+//        // Mail Server 설정
+//        String charSet = "utf-8";
+//        String hostSMTP = "smtp.naver.com";
+//        String hostSMTPid = "이메일 입력";
+//        String hostSMTPpwd = "비밀번호 입력";
+//
+//        // 보내는 사람 EMail, 제목, 내용
+//        String fromEmail = "이메일 입력";
+//        String fromName = "Spring Homepage";
+//        String subject = "";
+//        String msg = "";
+//
+//        if(div.equals("find_pw")) {
+//            subject = "Spring Homepage 임시 비밀번호 입니다.";
+//            msg += "<div align='center' style='border:1px solid black; font-family:verdana'>";
+//            msg += "<h3 style='color: blue;'>";
+//            msg += users.getStudentIdx() + "님의 임시 비밀번호 입니다. 비밀번호를 변경하여 사용하세요.</h3>";
+//            msg += "<p>임시 비밀번호 : ";
+//            msg += users.getPassword() + "</p></div>";
+//        }
+//        // 받는 사람 E-Mail 주소
+//        String mail = users.getEmail();
+//        try {
+//            HtmlEmail email = new HtmlEmail();
+//            email.setDebug(true);
+//            email.setCharset(charSet);
+//            email.setSSL(true);
+//            email.setHostName(hostSMTP);
+//            email.setSmtpPort(587);
+//
+//            email.setAuthentication(hostSMTPid, hostSMTPpwd);
+//            email.setTLS(true);
+//            email.addTo(mail, charSet);
+//            email.setFrom(fromEmail, fromName, charSet);
+//            email.setSubject(subject);
+//            email.setHtmlMsg(msg);
+//            email.send();
+//        } catch (Exception e) {
+//            System.out.println("메일발송 실패 : " + e);
+//        }
+//    }
 
 }
---
+++ src/main/webapp/WEB-INF/views/findPws.jsp
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+
+    <title>Login</title>
+
+    <!-- Required Stylesheets -->
+    <link type="text/css" rel="stylesheet" href="https://unpkg.com/bootstrap/dist/css/bootstrap.min.css"/>
+    <link type="text/css" rel="stylesheet" href="https://unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.css"/>
+
+    <!-- Required scripts -->
+    <script src="https://unpkg.com/vue"></script>
+    <script src="https://unpkg.com/babel-polyfill@latest/dist/polyfill.min.js"></script>
+    <script src="https://unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.js"></script>
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
+</head>
+
+<body>
+<form name="frm" action="login" method="post">
+    <!-- Our application root element -->
+    <div id="app">
+        <b-container>
+            <b-jumbotron header="SKHU VOTE">
+                <p class="lead">성공회대학교 투표 서비스</p>
+            </b-jumbotron>
+        </b-container>
+    </div>
+</form>
+
+<!-- Start running your app -->
+
+
+</body>
+</html>
