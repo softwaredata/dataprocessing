c45deb1591abdcc1db93dd37569ed1b3d7dda85e
meeansub <welcltm@gmail.com>
2019-05-11 00:59:34 +0900

    #12 이메일 전송을 위한 클래스와 서비스클래스

--- src/main/java/net/skhu/email/Email.java
+++ src/main/java/net/skhu/email/Email.java
@@ -1,27 +1,16 @@
 package net.skhu.email;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Email {
-    private String subject;
-    private String content;
-    private String receiver;
+    private String sender;//보내는사람
 
-    public String getReceiver() {
-        return receiver;
-    }
-    public void setReceiver(String receiver) {
-        this.receiver = receiver;
-    }
+    private String recipient;//받는사람
 
-    public String getSubject() {
-        return subject;
-    }
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-    public String getContent() {
-        return content;
-    }
-    public void setContent(String content) {
-        this.content = content;
-    }
+    private String subject;//제목
+
+    private String content;//내용
 }
