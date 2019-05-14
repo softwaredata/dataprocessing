4836604acb7be1fd8169f05aabf93f33c8b89af3
bomi <a51022866@gmail.com>
2019-05-14 19:52:37 +0900

    #3 Create JwtProvider

---
+++ src/main/java/net/skhu/config/jwt/provider/JwtProvider.java
@@ -0,0 +1,34 @@
+package net.skhu.config.jwt.provider;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.skhu.config.jwt.JwtDecoder;
+import net.skhu.config.jwt.token.JwtPreProcessingToken;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by bomi on 2019-05-14.
+ */
+
+@Slf4j
+@AllArgsConstructor
+@Component
+public class JwtProvider implements AuthenticationProvider {
+
+    private final JwtDecoder jwtDecoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String token = (String)authentication.getPrincipal();
+
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return JwtPreProcessingToken.class.isAssignableFrom(authentication);
+    }
+}
