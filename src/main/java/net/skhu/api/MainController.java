6cf9d1e1912de7a877e4b86bb5a415258425a122
meeansub <welcltm@gmail.com>
2019-06-06 23:26:14 +0900

    controller update

--- src/main/java/net/skhu/api/MainController.java
+++ src/main/java/net/skhu/api/MainController.java
@@ -3,7 +3,9 @@ package net.skhu.api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -18,4 +20,18 @@ public class MainController {
 
         return "main/main";
     }
+
+    @GetMapping("election/{vote}")
+    public String election(Model model, @PathVariable("vote") int vote){
+        model.addAttribute("vote",vote);
+        return "election/election1";
+    }
+
+    @GetMapping("teamDetail/{vote}")
+    public String teamDetail(Model model, @PathVariable("vote") int vote){
+        model.addAttribute("vote",vote);
+        return "election/teamDetail";
+    }
+
+
 }
