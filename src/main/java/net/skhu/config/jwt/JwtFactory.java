c631cee13bfaa7747e082bdabdd74f63fc58e29a
bomi <a51022866@gmail.com>
2019-06-10 13:20:53 +0900

    Update for method creating token

--- src/main/java/net/skhu/config/jwt/JwtFactory.java
+++ src/main/java/net/skhu/config/jwt/JwtFactory.java
@@ -3,6 +3,7 @@ package net.skhu.config.jwt;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import lombok.extern.slf4j.Slf4j;
+import net.skhu.config.security.MemberContext;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -22,13 +23,13 @@ public class JwtFactory {
     @Value("${JWT.SECRET}")
     private static String SECRET;
 
-    public String create() {
+    public String create(MemberContext memberContext) {
         String token = null;
         try {
             token = JWT.create()
                     .withIssuer(ISSUER)
-                    //.withClaim()
-                    //.withClaim()
+                    .withClaim("USERNAME", memberContext.getUsername())
+                    .withClaim("USER_ROLE", memberContext.getAuthorities().toString())
                     .sign(Algorithm.HMAC256(SECRET));
 
         } catch(UnsupportedEncodingException e) {
